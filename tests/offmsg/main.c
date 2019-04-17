4cd332c412ee1868c824a7a15b83d7fdafcdc3bb
stiartsly <stiartsly@gmail.com>
2019-04-17 16:58:51 +0800

    Refinement [skip CI]

--- tests/offmsg/main.c
+++ tests/offmsg/main.c
@@ -35,89 +35,28 @@ static const char *config_files[] = {
     NULL
 };
 
-#define STREQ(a, b) (strcmp((a), (b)) == 0)
-
 typedef enum RUNNING_MODE {
     UNKNOWN_MODE,
     SENDER_MODE,
     RECEIVER_MODE
 } RUNNING_MODE;
 
-static void output(const char *format, va_list args)
-{
-    // Do nothing.
-}
-
-const char *presence_name[] = {
-    "none",    // None;
-    "away",    // Away;
-    "busy",    // Busy;
+const char *_str[] = {
+    "unknown",
+    "sender",
+    "receiver"
 };
 
-static int first_friends_item = 1;
+static void output_null(const char *format, va_list args) {}
 
-static const char *connection_name[] = {
-    "online",
-    "offline"
-};
-
-/* This callback share by list_friends and global
- * friend list callback */
-static bool friends_list_callback(ElaCarrier *w, const ElaFriendInfo *friend_info,
-                                 void *context)
+static void output_error()
 {
-    static int count;
-
-    if (first_friends_item) {
-        count = 0;
-        vlogI("Friend list from carrier network:\n");
-        vlogI("  %-46s %8s %s\n", "ID", "Connection", "Label");
-        vlogI("  %-46s %8s %s\n", "----------------", "----------", "-----");
-    }
-
-    if (friend_info) {
-        vlogI("  %-46s %8s %s\n", friend_info->user_info.userid,
-               connection_name[friend_info->status], friend_info->label);
-        first_friends_item = 0;
-        count++;
-    } else {
-        /* The list ended */
-        vlogI("  ----------------\n");
-        vlogI("Total %d friends.\n", count);
-
-        first_friends_item = 1;
-    }
-
-    return true;
+    printf("error:error\n");
 }
 
-/* This callback share by list_friends and global
- * friend list callback */
-static void display_friend_info(const ElaFriendInfo *fi)
+static void output_addr_userid(const char *addr, const char *userid)
 {
-    vlogI("           ID: %s\n", fi->user_info.userid);
-    vlogI("         Name: %s\n", fi->user_info.name);
-    vlogI("  Description: %s\n", fi->user_info.description);
-    vlogI("       Gender: %s\n", fi->user_info.gender);
-    vlogI("        Phone: %s\n", fi->user_info.phone);
-    vlogI("        Email: %s\n", fi->user_info.email);
-    vlogI("       Region: %s\n", fi->user_info.region);
-    vlogI("        Label: %s\n", fi->label);
-    vlogI("     Presence: %s\n", presence_name[fi->presence]);
-    vlogI("   Connection: %s\n", connection_name[fi->status]);
-}
-
-static void friend_added_callback(ElaCarrier *w, const ElaFriendInfo *info,
-                                  void *context)
-{
-    vlogI("New friend added:\n");
-    display_friend_info(info);
-}
-
-static void friend_removed_callback(ElaCarrier *w, const char *friendid,
-                                    void *context)
-{
-    vlogI("Friend %s removed!\n", friendid);
+    printf("%s:%s\n", addr, userid);
 }
 
 static void idle_callback(ElaCarrier *w, void *context)
@@ -141,13 +80,6 @@ static void connection_callback(ElaCarrier *w, ElaConnectionStatus status,
     }
 }
 
-static void friend_info_callback(ElaCarrier *w, const char *friendid,
-                                 const ElaFriendInfo *info, void *context)
-{
-    vlogI("Friend information changed:\n");
-    display_friend_info(info);
-}
-
 static void friend_connection_callback(ElaCarrier *w, const char *friendid,
                                        ElaConnectionStatus status, void *context)
 {
@@ -165,38 +97,37 @@ static void friend_connection_callback(ElaCarrier *w, const char *friendid,
     }
 }
 
-static void friend_request_callback(ElaCarrier *w, const char *userid,
-                                    const ElaUserInfo *info, const char *hello,
-                                    void *context)
-{
-    vlogI("Friend request from user[%s] with HELLO: %s.\n",
-           *info->name ? info->name : userid, hello);
-}
-
 static void message_callback(ElaCarrier *w, const char *from,
                              const void *msg, size_t len, void *context)
 {
     vlogI("Message from friend[%s]: %.*s\n", from, (int)len, msg);
 }
 
-static void ready_callback(ElaCarrier *w, void *context)
-{
-    vlogI("ready_callback invoked\n");
-}
-
 static void usage(void)
 {
-    printf("Carrier offline message tests.\n");
-    printf("\n");
-    printf("Run offline message tests.\n");
-    printf("Usage: offmsg_tests -c CONFIG\n");
-    printf("\n");
-    printf("Launch offline message sender\n");
-    printf("Usage: offmsg_tests -c CONFIG --sender --from=YOUR-PATH [--addr=REMOTE-ADDR --uid=REMOTE-USERID]\n");
-    printf("\n");
-    printf("Launch offline message receiver\n");
-    printf("Usage: offmsg_tests -c CONFIG --receiver --to=YOUR-PATH\n");
-    printf("\n");
+    printf("usage: offmsg_tests [option] --sender | --receiver.\n"
+           "\n"
+           "    -c, --config[=<config-path>\n"
+           "                     test pathname of config file\n"
+           "    --init           create carrier instance, and print its \n"
+           "                     address and userid\n"
+           "\n"
+           "    --sender         the role to send offline messages\n"
+           "    --receiver       the role to receive offline messages\n"
+           "\n"
+           "    -a, --remote-address=<remote-address>\n"
+           "                     the address of remote tests node\n"
+           "    -u, --remote-userid=<remote-userid>\n"
+           "                     the userid of remote tests node\n"
+           "\n"
+           "    --refmsg-from=<offmsg-text>\n"
+           "                     the text file that sender read from and send\n"
+           "                     it as offline message to remote tests node"
+           "    --refmsg-to=<offmsg-text>\n"
+           "                     the text file that receiver store offline\n"
+           "                     offline message into after receiving them\n"
+           "                     them from tests node\n"
+           "\n");
 }
 
 #ifdef HAVE_SYS_RESOURCE_H
@@ -228,28 +159,6 @@ static void signal_handler(int signum)
     exit(-1);
 }
 
-static void log_init(TestConfig *test_config, int mode)
-{
-    char filename[PATH_MAX];
-    char *log_file;
-    int level;
-
-    if (mode == SENDER_MODE) {
-        sprintf(filename, "%s/sender.log", test_config->data_location);
-        log_file = filename;
-        level = test_config->sender_log_level;
-    } else if (mode == RECEIVER_MODE) {
-        sprintf(filename, "%s/receiver.log", test_config->data_location);
-        log_file = filename;
-        level = test_config->receiver_log_level;
-    } else {
-        log_file = NULL;
-        level = VLOG_INFO;
-    }
-
-    vlog_init(level, log_file, output);
-}
-
 const char *get_config_path(const char *config_file, const char *config_files[])
 {
     const char **file = config_file ? &config_file : config_files;
@@ -273,14 +182,6 @@ const char *get_config_path(const char *config_file, const char *config_files[])
     return NULL;
 }
 
-void print_user_info(char *address, char *id)
-{
-    if (!address || !id)
-        printf("error:error\n");
-    else
-        printf("%s:%s\n", address, id);
-}
-
 int main(int argc, char *argv[])
 {
     ElaOptions opts = {0};
@@ -289,8 +190,9 @@ int main(int argc, char *argv[])
     RUNNING_MODE mode = UNKNOWN_MODE;
     const char *config_file = NULL;
     char address[ELA_MAX_ADDRESS_LEN + 1] = {0};
-    char id[ELA_MAX_ID_LEN + 1] = {0};
+    char userid[ELA_MAX_ID_LEN + 1] = {0};
     char datadir[PATH_MAX] = {0};
+    char logfile[PATH_MAX] = {0};
     int log_level = 0;
     int rc = 0;
     int i = 0;
@@ -298,6 +200,7 @@ int main(int argc, char *argv[])
     int generate_info = 0;
     int opt = 0;
     int idx = 0;
+    int level;
     struct option options[] = {
         { "sender",         no_argument,        NULL, 1 },
         { "receiver",       no_argument,        NULL, 2 },
@@ -320,9 +223,7 @@ int main(int argc, char *argv[])
         case 1:
         case 2:
             if (mode != UNKNOWN_MODE) {
-                vlogE("Error: Conflict arguments.\n");
-                print_user_info(NULL, NULL);
-                usage();
+                output_error();
                 return -1;
             }
 
@@ -343,7 +244,6 @@ int main(int argc, char *argv[])
 
         case 'h':
         case '?':
-            print_user_info(NULL, NULL);
             usage();
             return -1;
         }
@@ -352,50 +252,50 @@ int main(int argc, char *argv[])
     if (debug)
         wait_for_debugger_attach();
 
+    if (mode == UNKNOWN_MODE) {
+        output_error();
+        return -1;
+    }
+
 #if defined(_WIN32) || defined(_WIN64)
     WORD wVersionRequested;
     WSADATA wsaData;
 
     wVersionRequested = MAKEWORD(2, 0);
 
-    if (WSAStartup(wVersionRequested, &wsaData) != 0)
+    if (WSAStartup(wVersionRequested, &wsaData) != 0) {
+        output_error();
         return -1;
+    }
 #endif
 
     config_file = get_config_path(config_file, config_files);
 
-    if (!config_file) {
-        vlogE("Error: Missing config file.\n");
-        print_user_info(NULL, NULL);
-        usage();
+    if (!config_file || mode == UNKNOWN_MODE) {
+        vlogE("Error: Missing config file");
+        output_error();
         return -1;
      }
 
-    if (mode == UNKNOWN_MODE) {
-        print_user_info(NULL, NULL);
-        usage();
-        return -1;
-    }
-
-    // The primary job: load configuration file
     global_config = load_config(config_file);
-    log_init(global_config, mode);
 
     memset(&opts, 0, sizeof(opts));
+    sprintf(logfile, "%s/%s.log", global_config->data_location, _str[mode]);
+
+    if (mode == SENDER_MODE)
+        level = global_config->sender_log_level;
+    else
+        level = global_config->receiver_log_level;
+
+    vlog_init(level, logfile, output_null);
+
     opts.udp_enabled = global_config->udp_enabled;
-    if (mode == SENDER_MODE) {
-        log_level = global_config->sender_log_level;
-        sprintf(datadir, "%s/sender", global_config->data_location);
-    } else {
-        log_level = global_config->receiver_log_level;
-        sprintf(datadir, "%s/receiver", global_config->data_location);
-    }
+    sprintf(datadir, "%s/%s", global_config->data_location, _str[mode]);
     opts.persistent_location = datadir;
     opts.dht_bootstraps_size = global_config->bootstraps_size;
     opts.dht_bootstraps = (DhtBootstrapNode *)calloc(1, sizeof(DhtBootstrapNode) * opts.dht_bootstraps_size);
     if (!opts.dht_bootstraps) {
-        vlogE("out of memory.");
-        print_user_info(NULL, NULL);
+        output_error();
         return -1;
     }
 
@@ -412,8 +312,7 @@ int main(int argc, char *argv[])
     opts.hive_bootstraps_size = global_config->hive_bootstraps_size;
     opts.hive_bootstraps = (HiveBootstrapNode *)calloc(1, sizeof(HiveBootstrapNode) * opts.hive_bootstraps_size);
     if (!opts.hive_bootstraps) {
-        vlogE("out of memory.");
-        print_user_info(NULL, NULL);
+        output_error();
         free(opts.dht_bootstraps);
         return -1;
     }
@@ -430,33 +329,24 @@ int main(int argc, char *argv[])
     memset(&callbacks, 0, sizeof(callbacks));
     callbacks.idle = idle_callback;
     callbacks.connection_status = connection_callback;
-    callbacks.friend_list = friends_list_callback;
     callbacks.friend_connection = friend_connection_callback;
-    callbacks.friend_info = friend_info_callback;
-    callbacks.friend_presence = NULL;
-    callbacks.friend_request = friend_request_callback;
-    callbacks.friend_added = friend_added_callback;
-    callbacks.friend_removed = friend_removed_callback;
     callbacks.friend_message = message_callback;
-    callbacks.friend_invite = NULL;
-    callbacks.ready = ready_callback;
 
     w = ela_new(&opts, &callbacks, NULL);
     free(opts.dht_bootstraps);
     free(opts.hive_bootstraps);
 
     if (!w) {
-        vlogE("Error create carrier instance: 0x%x\n", ela_get_error());
-        vlogE("Press any key to quit...");
-        print_user_info(NULL, NULL);
+        vlogE("Create carrier instance error: 0x%x\n", ela_get_error());
+        output_error();
         rc = -1;
         goto quit;
     }
 
     if (generate_info) {
         ela_get_address(w, address, sizeof(address));
-        ela_get_userid(w, id, sizeof(id));
-        print_user_info(address, id);
+        ela_get_userid(w, userid, sizeof(userid));
+        output_addr_userid(address, userid);
         ela_kill(w);
         rc = 0;
         goto quit;
@@ -464,8 +354,7 @@ int main(int argc, char *argv[])
 
     rc = ela_run(w, 10);
     if (rc != 0) {
-        vlogE("Error start carrier loop: 0x%x\n", ela_get_error());
-        vlogE("Press any key to quit...");
+        vlogE("Run carrier instance error: 0x%x\n", ela_get_error());
         ela_kill(w);
         rc = -1;
         goto quit;
